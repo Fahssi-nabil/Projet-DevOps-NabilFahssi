bc21141431f9705609c6d8b8800b518b7b8441f4
attariuas hicham <hattarius@uae.ac.ma>
2026-01-11 17:20:32 +0000

    Ajout de commentaires sur la branche dev

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -1,5 +1,9 @@
 package com.devops;
 
+/**
+ * Application DevOps Mini Projet
+ * Auteur: Nabil Fahssi
+ */
 public class App {
     public static void main(String[] args) {
         System.out.println("Bonjour et bon courage dans votre projet en DevOps");
